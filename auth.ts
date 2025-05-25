29601a10cb4a4c811cc67f14f0c50b320c70dead
srivastavya02 <srivastavya24@gmail.com>
2025-05-25 21:57:19 +0530

    setup oauth and prisma

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
- 
+import Twitter from "next-auth/providers/twitter"
+import Google from "next-auth/providers/google"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [Twitter, Google],
+});
