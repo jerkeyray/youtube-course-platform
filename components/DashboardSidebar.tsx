95189ae5514d9f6b7681d63daee7cfda746391d3
srivastavya02 <srivastavya24@gmail.com>
2025-05-27 19:52:01 +0530

    remove uneccessary files

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+
 import {
   LayoutDashboard,
   BookOpen,
